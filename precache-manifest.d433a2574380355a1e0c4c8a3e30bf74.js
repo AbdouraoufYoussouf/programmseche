75dcdb82d6b9b4f615388a4e197285423c40e633
Abdouraouf <youssouf.abdouraouf.4@gmail.com>
2022-05-06 02:11:30 +0200

    Updates

---
+++ precache-manifest.d433a2574380355a1e0c4c8a3e30bf74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6468b0e93f4c7f208441",
+    "url": "/programmseche/static/css/main.85bee462.chunk.css"
+  },
+  {
+    "revision": "6468b0e93f4c7f208441",
+    "url": "/programmseche/static/js/main.a8bf3cff.chunk.js"
+  },
+  {
+    "revision": "509425a3d368006c179e",
+    "url": "/programmseche/static/js/runtime~main.8c9ffd14.js"
+  },
+  {
+    "revision": "9accdfed734846900b08",
+    "url": "/programmseche/static/js/2.3dc675dd.chunk.js"
+  },
+  {
+    "revision": "749ea7409148b57e4a6ee2aec13e1628",
+    "url": "/programmseche/static/media/doc.749ea740.PNG"
+  },
+  {
+    "revision": "0e8392798a3ec879d9720f6d1f4e921b",
+    "url": "/programmseche/static/media/raf.0e839279.jpg"
+  },
+  {
+    "revision": "c359cbbbda48b8006fa01e3eb082b6d5",
+    "url": "/programmseche/index.html"
+  }
+];
